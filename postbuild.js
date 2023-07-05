10afb6812504ff779f347bcae91efe7154eb7b10
m.ali petek <malipetek@gmail.com>
2023-07-05 08:02:40 +0300

    folder included with the move

--- postbuild.js
+++ postbuild.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const { moveRemoveSync } = require('rimraf');
@@ -19,25 +19,28 @@ files.filter(fileOrFolder => fileOrFolder.endsWith('.html')).forEach(file => {
   const data = fs.readFileSync(htmlFilePath, 'utf8');
 
   // Find script tags matching the pattern
-  const newData = data.replace(/<link\s+(?:[^>]+\s+)?href="\/(js\/common|assets)\/([^"]+)"/g, (all, typeMatch, match) => {
-    const type = typeMatch === 'js/common' ? 'js' : 'css';
-
+  const newData = data.replace(/<link\s+(?:[^>]+\s+)?href="(\/js\/common\/|\/assets\/)([^"]+)"/g, (all, folder, match) => {
     const jsOrCssFileName = match;
-    const jsFilePath = path.join(jsDirectory, 'common', jsOrCssFileName);
-    const cssFilePath = path.join(cssDirectory, jsOrCssFileName);
-
+    console.log('moving ', folder, match);
+    
+    const filePath = path.join(htmlDirectory, folder, jsOrCssFileName);
     // Copy JS file to the new directory
-    const targetFilePath = path.join(targetFolder, jsOrCssFileName);
+    const targetFilePath = path.join(targetFolder, folder, jsOrCssFileName);
+    const targetFolderPath = path.join(targetFolder, folder);
+
     mkdirp.sync(targetFolder); // Create target folder if it doesn't exist
-    fs.copyFileSync(type === 'js' ? jsFilePath : cssFilePath, targetFilePath);
-    return all.replace(/href="\/(js\/common|assets)\/([^"]+)"/, 'href="$2"');
+    
+    fs.ensureDirSync(targetFolderPath);
+    fs.copyFileSync(filePath, targetFilePath);
+    
+    return all.replace(/href="\/(js\/common|assets)\/([^"]+)"/, 'href="$1/$2"');
   })
-    .replace(/<script\s+(?:[^>]+\s+)?src="\/js\/([^"]+)"/g, (all) => all.replace(/src="\/js\/([^"]+)"/, 'src="$1"'))
+    .replace(/<script\s+(?:[^>]+\s+)?src="\/js\/([^"]+)"/g, (all) => all.replace(/src="\/(js\/[^"]+)"/, 'src="$1"'))
     .replace('<head>', `<head><base href="/${file.replace('.html', '')}/">`);
 
   const jsEntryFile = path.join(jsDirectory, file.replace('.html', '.js'));
 
-  fs.copyFileSync(jsEntryFile, path.join(targetFolder, file.replace('.html', '.js')));
+  fs.copyFileSync(jsEntryFile, path.join(targetFolder, 'js', file.replace('.html', '.js')));
 
   // move html to target folder
   const targetHtmlFilePath = path.join(targetFolder, 'index.html');
